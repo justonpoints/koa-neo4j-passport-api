335489e854a684856cc9d031e6d8bf1c729492f4
Juston Points <justonpoints@gmail.com>
2015-06-24 22:10:20 -0700

    seperated database information into its own file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //node libraries, should also be contained in the /package.json file
 var koa = require('koa');
 var koa_router = require('koa-router');
+var request = require('koa-request');//move this to the index.js file
 var session = require('koa-generic-session');
 var mount = require('koa-mount');
 var logger = require('koa-logger');
@@ -12,8 +13,9 @@ var local = require('passport-local').Strategy;
 
 //local libraries
 var nodes = require('./lib/lib.js');
-var auth = require('./lib/auth')(passport,local)//authenticator
-var api = require('./lib/api/api.js');//main api, requires an authenticated session
+var neo4j = require('./lib/neo4j.js')(request);
+var auth = require('./lib/auth')(passport,local);//authenticator
+var api = require('./lib/api/api.js')(neo4j);//main api, requires an authenticated session
 var public_api = require('./lib/api/public_api.js')(auth);//api that can accessed without an active session.
 
 //initializers
@@ -25,11 +27,11 @@ var public_router = new koa_router();//for the non-authenicated api.
 //The component order is important.
 //__________________________________________________________________ 
 
+//Unkown Error handler:500
+nodes.error(app);
+
 //logger
 nodes.logger(app,logger,true);
-
-//Unkown Error handler 
-nodes.error(app);
 
 //limiter
 nodes.limiter(app,limit);
@@ -43,6 +45,9 @@ nodes.public_routes(app,public_router,mount,public_api);
 //The following routes require a valid session.
 nodes.routes(app,router,mount,api);
 
-if (!module.parent) app.listen(4000);//move the port to a config file.
+if (!module.parent) app.listen(1337);//move the port to a config file.
 console.log('the Api is running on port 1337');
 //__________________________________________________________________
+
+
+  
