405782db77ed25e817ee10dfc7c245d6d69ed9c9
justonpoints <justonpoints@gmail.com>
2015-06-24 18:29:00 -0700

    changed components to nodes - nodes seems to better characterize what koa is doing.

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ var session = require('koa-generic-session');
 var mount = require('koa-mount');
 var logger = require('koa-logger');
 var limit = require('koa-better-ratelimit');
+
+
 var passport = require('koa-passport');
 var local = require('passport-local').Strategy;
 
 //local libraries
-var components = require('./lib/lib.js');
+var nodes = require('./lib/lib.js');
 var auth = require('./lib/auth')(passport,local)//authenticator
 var api = require('./lib/api/api.js');//main api, requires an authenticated session
 var public_api = require('./lib/api/public_api.js')(auth);//api that can accessed without an active session.
@@ -24,23 +26,23 @@ var public_router = new koa_router();//for the non-authenicated api.
 //__________________________________________________________________ 
 
 //logger
-components.logger(app,logger,true);
+nodes.logger(app,logger,true);
 
 //Unkown Error handler 
-components.error(app);
+nodes.error(app);
 
 //limiter
-components.limiter(app,limit);
+nodes.limiter(app,limit);
 
 //session handeler
-components.session(app,session,auth);
+nodes.session(app,session,auth);
 
 //route accessible without a session. This includes login and log out.
-components.public_routes(app,public_router,mount,public_api);
+nodes.public_routes(app,public_router,mount,public_api);
 
 //The following routes require a valid session.
-components.routes(app,router,mount,api);
+nodes.routes(app,router,mount,api);
 
-if (!module.parent) app.listen(1337);//move the port to a config file.
+if (!module.parent) app.listen(4000);//move the port to a config file.
 console.log('the Api is running on port 1337');
 //__________________________________________________________________
